093e436f3e65e0793a7c158ab77981a8891bd300
TALHA441275 <talhamercan2004@hotmail.com>
2025-08-06 23:59:55 +0300

    Root files have been completed.

--- runner.sh
+++ runner.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+if [ "$(uname)" != "Windows" ] && ! command pip show uvloop &> /dev/null; then
+    pip3 install uvloop
+fi
+
 uvicorn src:app --reload --proxy-headers --host 0.0.0.0 --port 8000
